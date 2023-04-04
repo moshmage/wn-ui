950b06945c7184ee6034d5e84688c0c35eebde44
MoshMage <moshmage@gmail.com>
2023-04-04 12:04:51 +0100

    add simple page test example

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+    reactRemoveProperties: true,
+    removeConsole: {
+      exclude: ['error'],
+    },
+  }
 }
 
 module.exports = nextConfig
