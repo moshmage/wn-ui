34cabe0fcd8819c84f298308a091a4959949f510
MoshMage <moshmage@gmail.com>
2023-04-27 15:45:28 +0100

    test use-tokens-list

---
+++ src/__tests__/x-hooks/use-tokens-list.test.ts
@@ -0,0 +1,32 @@
+import {renderHook} from "@testing-library/react";
+import {setTokensList, useTokensList} from "@/x-hooks/use-tokens-list";
+
+describe(`useTokensList()`, () => {
+
+  it(`expects empty list`, () => {
+    const {result: {current}} = renderHook(() => useTokensList());
+    expect(Object.keys(current).length).toBeFalsy();
+  })
+
+  it(`expects addition of one`, () => {
+    const {result: {current}} = renderHook(() => setTokensList);
+    const {result: {current: list}} = renderHook(() => useTokensList());
+
+    current({type: 0, payload: {name: `name`, symbol: `symbol`, contractAddress: `0x1`, totalSupply: 10}});
+    expect(list['0x1']).toBeTruthy();
+  })
+
+  it(`expects setting of token`, () => {
+    const {result: {current}} = renderHook(() => setTokensList);
+    const {result: {current: list}} = renderHook(() => useTokensList());
+    current({type: 2, payload: {token: {name: 'name'},contractAddress: '0x1'}});
+    expect(list['0x1'].token).not.toBeFalsy();
+  })
+
+  it(`expects removal`, () => {
+    const {result: {current}} = renderHook(() => setTokensList);
+    const {result: {current: list}} = renderHook(() => useTokensList());
+    current({type: 1, payload: {contractAddress: `0x1`}});
+    expect(list['0x1']).toBeFalsy();
+  });
+})
