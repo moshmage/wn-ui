a2965cab098b95d955b4989e3d467d28126acf58
MoshMage <moshmage@gmail.com>
2023-05-03 00:14:38 +0100

    expect before changes on useTokensList

--- src/__tests__/x-hooks/use-tokens-list.test.ts
+++ src/__tests__/x-hooks/use-tokens-list.test.ts
@@ -12,6 +12,7 @@ describe(`useTokensList()`, () => {
     const {result: {current}} = renderHook(() => setTokensList);
     const {result: {current: list}} = renderHook(() => useTokensList());
 
+    expect(list['0x1']).not.toBeTruthy();
     current({type: 0, payload: {name: `name`, symbol: `symbol`, contractAddress: `0x1`, totalSupply: 10}});
     expect(list['0x1']).toBeTruthy();
   })
@@ -19,6 +20,8 @@ describe(`useTokensList()`, () => {
   it(`expects setting of token`, () => {
     const {result: {current}} = renderHook(() => setTokensList);
     const {result: {current: list}} = renderHook(() => useTokensList());
+
+    expect(list['0x1'].token).toBeFalsy();
     current({type: 2, payload: {token: {name: 'name'},contractAddress: '0x1'}});
     expect(list['0x1'].token).not.toBeFalsy();
   })
@@ -26,6 +29,8 @@ describe(`useTokensList()`, () => {
   it(`expects removal`, () => {
     const {result: {current}} = renderHook(() => setTokensList);
     const {result: {current: list}} = renderHook(() => useTokensList());
+
+    expect(list['0x1']).not.toBeFalsy();
     current({type: 1, payload: {contractAddress: `0x1`}});
     expect(list['0x1']).toBeFalsy();
   });
